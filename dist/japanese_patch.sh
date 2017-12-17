2d89ad385b3b98a80c03b79d59454879aa1c4e5f
Uhucream <aborog1324@yahoo.co.jp>
2017-12-17 10:10:25 +0900

    Delete japanese_patch.sh

--- dist/japanese_patch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-env LC_ALL=C LANG=C LANGUAGE=en bash
-sed -e "27a export LANG="ja_JP.UTF-8"
-sed -e "28a export LC_ALL="ja_JP.UTF-8""
-sed -e "29a export LANGUAGE="ja""
-apt-get update
-apt-get install locale mlterm fonts-noto fonts-vlgothic fonts-horai-umefont fonts-umeplus
